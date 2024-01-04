aba17be8a01e3894999656e2f83ed31fc72a1e4e
Ankit Pandey <ankitpandey.272003@gmail.com>
2024-01-04 20:23:15 +0530

    imported dotenv config file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
+import dotenv from 'dotenv';
 import { SnackbarProvider } from 'notistack'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SnackbarProvider>

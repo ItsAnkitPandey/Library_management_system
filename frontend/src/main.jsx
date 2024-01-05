459d528fee23a346679edff26d2feb17c24e6b0b
Ankit Pandey <ankitpandey.272003@gmail.com>
2024-01-05 08:41:51 +0530

    removed env imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,7 @@
-import dotenv from 'dotenv';
 import { SnackbarProvider } from 'notistack'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SnackbarProvider>

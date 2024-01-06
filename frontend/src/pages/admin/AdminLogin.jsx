5c46cbd43341b024f73e70e5c89383c24d28b178
Ankit Pandey <ankitpandey.272003@gmail.com>
2024-01-06 09:05:17 +0530

    removed unneccesary div

--- frontend/src/pages/admin/AdminLogin.jsx
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -65,7 +65,6 @@ const AdminLogin = ({onlogin}) => {
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
             <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username" value={username} onChange={handleUsernameChange} required />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
